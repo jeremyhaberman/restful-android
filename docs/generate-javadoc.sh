983c49d33a1a1eef153a925e7ea3d5742e773215
Jeremy Haberman <jeremy@livefront.com>
2012-01-28 20:57:52 -0600

    Fix issues with javadoc and provide example syntax

--- docs/generate-javadoc.sh
+++ docs/generate-javadoc.sh
@@ -5,4 +5,5 @@ javadoc \
 -doctitle "RESTful Android" \
 -windowtitle "RESTful Android" \
 -sourcepath ../src com.jeremyhaberman.restfulandroid \
+-classpath $1 \
 -subpackages com.jeremyhaberman.restfulandroid.activity:com.jeremyhaberman.restfulandroid.provider:com.jeremyhaberman.restfulandroid.security:com.jeremyhaberman.restfulandroid.service:com.jeremyhaberman.restfulandroid.util
